48133968976c07769b521b03b813ea500c03896d
jhonRawls <jhonRawls@gmail.com>
2016-10-03 20:43:18 +0800

    服务消费者，服务提供者--注册中心--服务消费者，将服务拆解，类实现微服务架构，和快速的分布式开发，国内有dubbo

--- src/main/java/com/dada/controller/Application.java
+++ src/main/java/com/dada/controller/Application.java
@@ -2,14 +2,27 @@ package com.dada.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 @EnableAutoConfiguration // 标志在对应的主要class上
 @Configuration
 @ComponentScan //允许boot依据约定的规则对项目中的bean进行扫描
+@EnableDiscoveryClient//注解来添加发现服务能力
 public class Application {
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 	 // 默认的main对应的class应该放在项目src代码的一级目录下,默认使用Applicati
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("web服务启动");
 	}
 }
--- src/main/java/com/dada/controller/UserLoginController.java
+++ src/main/java/com/dada/controller/UserLoginController.java
@@ -1,10 +1,12 @@
 package com.dada.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController  
 @EnableAutoConfiguration // 允许自动加载配置，Boot要采用一种特定的方式来对应用进行配置
@@ -18,4 +20,13 @@ public class UserLoginController {
 /*	public static void main(String[] args) {
 		SpringApplication.run(UserLoginController.class, args);
 	}*/
+	
+	@Autowired
+    RestTemplate restTemplate;
+	
+	 @RequestMapping(value = "/add", method = RequestMethod.GET)
+	    public String add() {
+	        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+	    }
+
 }
